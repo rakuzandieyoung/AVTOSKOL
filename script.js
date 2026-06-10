fa620dc9be0e6f7f6812d7f0df84f91d6d91f618
rakuzandieyoung <kakenovs72@gmail.com>
2026-06-11 01:20:57 +0600

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-document.getElementById("orderForm").addEventListener("submit", function(event){
+document.getElementById("orderForm").addEventListener("submit", function(e){
 
-    event.preventDefault();
+    e.preventDefault();
 
-    alert("Спасибо! Ваша заявка успешно отправлена.");
+    const name = document.querySelector('input[type="text"]').value;
+    const phone = document.querySelector('input[type="tel"]').value;
+    const service = document.querySelector('select').value;
+    const comment = document.querySelector('textarea').value;
 
-    this.reset();
+    const message =
+`Здравствуйте!
+
+Имя: ${name}
+Телефон: ${phone}
+Услуга: ${service}
+
+Комментарий:
+${comment}`;
+
+    const whatsappUrl =
+`https://wa.me/77788811124?text=${encodeURIComponent(message)}`;
+
+    window.open(whatsappUrl, "_blank");
 
 });
