3d1635cad8c268df39097d9b87f10750d9357688
rakuzandieyoung <kakenovs72@gmail.com>
2026-06-11 02:56:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("orderForm").addEventListener("submit", function(e){
 
     const name = document.querySelector('input[type="text"]').value;
     const phone = document.querySelector('input[type="tel"]').value;
-    const service = document.querySelector('select').value;
+    const service = document.querySelector('select').value || "Не выбрана";
     const comment = document.querySelector('textarea').value;
 
     const message =
@@ -21,5 +21,24 @@ ${comment}`;
 `https://wa.me/77788811124?text=${encodeURIComponent(message)}`;
 
     window.open(whatsappUrl, "_blank");
+    alert("Спасибо! Сейчас откроется WhatsApp для отправки заявки.");
 
 });
+
+const reviews = document.querySelectorAll(".review-card");
+
+let reviewIndex = 0;
+
+setInterval(() => {
+
+    reviews[reviewIndex].classList.remove("active");
+
+    reviewIndex++;
+
+    if(reviewIndex >= reviews.length){
+        reviewIndex = 0;
+    }
+
+    reviews[reviewIndex].classList.add("active");
+
+}, 5000);
